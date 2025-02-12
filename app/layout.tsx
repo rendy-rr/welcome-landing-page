2e29d3aa168a1d121ac7d815565cf814dc6c6207
rendy-rr <109682294+rendy-rr@users.noreply.github.com>
2025-02-12 15:00:29 +0700

    2nd update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/next"; 
 import "./globals.css";
 
 const geistSans = Geist({
@@ -31,6 +31,9 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <Analytics/>
+      <SpeedInsights/>
     </html>
+    
   );
 }
